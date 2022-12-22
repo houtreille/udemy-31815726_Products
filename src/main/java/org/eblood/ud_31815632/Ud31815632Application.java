5e8dfc0942971cbd1c74b5a94a67381676850724
mhoutreille <mhoutreille@pictet-technologies.com>
2022-12-22 10:47:17 +0100

    First commit

---
+++ src/main/java/org/eblood/ud_31815632/Ud31815632Application.java
@@ -0,0 +1,13 @@
+package org.eblood.ud_31815632;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud31815632Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ud31815632Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/eblood/ud_31815632/Ud31815632ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eblood.ud_31815632;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud31815632ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
