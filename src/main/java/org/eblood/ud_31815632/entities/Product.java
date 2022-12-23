b83a0d56ce2bbe289aa7a8038f60266257310eb8
mhoutreille <mhoutreille@pictet-technologies.com>
2022-12-23 15:53:08 +0100

    Implement repository

--- src/main/java/org/eblood/ud_31815632/entities/Product.java
+++ src/main/java/org/eblood/ud_31815632/entities/Product.java
@@ -1,19 +1,30 @@
 package org.eblood.ud_31815632.entities;
 
 
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
+@Entity
+@NoArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
     private String productName;
     private Double productPrice;
     private Date creationDate;
+
 }
---
+++ src/test/java/org/eblood/ud_31815632/repository/ProductRepositoryTest.java
@@ -0,0 +1,7 @@
+package org.eblood.ud_31815632.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+}
