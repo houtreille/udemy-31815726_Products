4c40e2636cf28ae8aa9575ae6c1e575e578760f7
mhoutreille <mhoutreille@pictet-technologies.com>
2023-01-05 14:45:19 +0100

    add rest on repo layer

--- src/main/java/org/eblood/ud_31815632/Ud31815632Application.java
+++ src/main/java/org/eblood/ud_31815632/Ud31815632Application.java
@@ -1,13 +1,25 @@
 package org.eblood.ud_31815632;
 
+import org.eblood.ud_31815632.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
-public class Ud31815632Application {
+public class Ud31815632Application implements CommandLineRunner {
+
+    @Autowired
+    RepositoryRestConfiguration repositoryRestConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(Ud31815632Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        repositoryRestConfiguration.exposeIdsFor(Product.class);
+    }
 }
